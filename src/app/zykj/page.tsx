1a1d66fd8e4a65d4070e7200dba590fc7f6ad566
Kom3ng <3262392248@qq.com>
2024-04-21 12:36:30 +0800

    feat: zykj main page

--- src/app/zykj/page.tsx
+++ src/app/zykj/page.tsx
@@ -1,7 +1,13 @@
-export default function Page(){
+import Link from "next/link";
+
+export default function Page() {
     return (
-        <div>
-            <h1>Page</h1>
+        <div className="min-h-screen bg-slate-50 dark:bg-gray-400">
+            <div className="px-20 space-y-4">
+                <div className="h-20"></div>
+                <p className="text-5xl font-bold">Zykj</p>
+                <p className="text-3xl font-bold">主页没写<Link href='/zykj/dash'>点这里先去dash吧</Link></p>
+            </div>
         </div>
     )
 }
