0c1a146d00d86b18f7bf27fd147541abd471903d
Kom3ng <3262392248@qq.com>
2024-05-04 13:51:31 +0800

    fix(api): fix api url

--- scripts/rewriteapi.js
+++ scripts/rewriteapi.js
@@ -8,7 +8,7 @@ const data = fs.readFileSync(apiFile, 'utf8')
     .replaceAll('appVersion: number','appVersion: number = 0');
 
 const baseData = fs.readFileSync(baseFile, 'utf8')
-    .replaceAll('http://sxz.api6.zykj.org','https://proxy.astrack.me/zykj');
+    .replaceAll('http://sxz.api.zykj.org','https://proxy.astrack.me/zykj');
 
 fs.writeFileSync(apiFile, data, 'utf8');
 fs.writeFileSync(baseFile, baseData, 'utf8');
